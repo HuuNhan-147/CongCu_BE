1c9894d882a1e9c84520a2ccb6c689f6a33ecd4e
HuuNhan-147 <caohuunhanhoabinh2003@gmail.com>
2026-04-01 11:31:07 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from "path";
+import routes from "./routes/index.js";
+
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json()); // ✅ Middleware JSON
+app.use(express.urlencoded({ extended: true })); // Xử lý dữ liệu form
+app.use("/api", routes); // ✅ Gọi routes
+app.get("/", (req, res) => {
+  res.send("Chào mừng bạn đến với API của tôi!");
+});
+// Cấu hình CORS
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+// Middleware xử lý lỗi 404 và các lỗi khác
+app.use((req, res, next) => {
+  const error = new Error("Không tìm thấy trang");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+    error: process.env.NODE_ENV === "development" ? error : {},
+  });
+});
