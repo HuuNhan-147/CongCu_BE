8a517399de8c696d0cbcb9a3b06407be8506516d
HuuNhan-147 <caohuunhanhoabinh2003@gmail.com>
2026-04-06 20:25:05 +0700

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://congcu-be-frontend-1-0.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
